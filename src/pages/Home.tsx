f7e5a586d849eebd2738b5d05318da960c79dca1
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-06-21 20:48:12 -0300

    Home function on Home.tsx updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,30 +3,32 @@ import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
 export function Home() {
-  <div>
-    <aside>
-      <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-      <strong>Crie salas Q&amp;A ao-vivo</strong>
-      <p>Tire as dúvidas da sua audiência em tempo real</p>
-    </aside>
-    <main>
-      <div>
-        <img src={logoImg} alt="Letmeask" />
-        <button>
-          <img src={googleIconImg} alt="Logo do Google" />
-          Crie sua sala com o Google
-        </button>
+  return (
+    <div>
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
         <div>
-          ou entre em uma sala
+          <img src={logoImg} alt="Letmeask" />
+          <button>
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div>
+            ou entre em uma sala
+          </div>
+          <form action="">
+            <input 
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <button type="submit">Entrar na sala</button>
+          </form>
         </div>
-        <form action="">
-          <input 
-            type="text"
-            placeholder="Digite o código da sala"
-          />
-          <button type="submit">Entrar na sala</button>
-        </form>
-      </div>
-    </main>
-  </div>
+      </main>
+    </div>
+  );
 }
