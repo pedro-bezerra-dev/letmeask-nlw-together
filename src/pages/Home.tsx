8ca084827c9c4d169a2509ea5109df87371e1a6b
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-06-22 13:46:29 -0300

    navigation between the created Home and New Room page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,42 @@
+import { useHistory } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
 export function Home() {
+  const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
+
+  console.log(user)
+
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle()
+    }
+    history.push('/rooms/new')
+  }
+
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>
+          <div className="separator">
             ou entre em uma sala
           </div>
           <form action="">
@@ -25,7 +44,7 @@ export function Home() {
               type="text"
               placeholder="Digite o código da sala"
             />
-            <button type="submit">Entrar na sala</button>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
