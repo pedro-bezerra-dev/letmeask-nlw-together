1f914efe64b3c54cb4138facd792e763b6ea2f5f
Pyter_dev <pyter_dev@pop-os.localdomain>
2021-06-23 15:22:05 -0300

    function to access an existing room created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -12,14 +14,30 @@ import '../styles/auth.scss';
 export function Home() {
   const history = useHistory()
   const { user, signInWithGoogle } = useAuth()
-
-  console.log(user)
+  const [roomCode, setRoomCode] = useState('')
 
   async function handleCreateRoom() {
     if(!user) {
       await signInWithGoogle()
     }
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if(roomCode.trim() === '') {
+      return
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if(!roomRef.exists()) {
+      alert('Room does not exists')
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`)
   }
 
   return (
@@ -39,10 +57,12 @@ export function Home() {
           <div className="separator">
             ou entre em uma sala
           </div>
-          <form action="">
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">Entrar na sala</Button>
           </form>
