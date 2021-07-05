bf4a6103ccb2554c17922e7307a791048354a79e
Pedro Henrique <pyter.dev@gmail.com>
2021-07-04 22:13:22 -0300

    now the global theme is provided from global.ts

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors.font};
+  }
+
+  body, input, button, textarea {
+    font: 400 1.6rem 'Roboto', sans-serif;
+  }
+`;
