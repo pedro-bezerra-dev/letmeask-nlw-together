9cc292327946c9329b95170ecbc6ea88e2f2915a
Pedro Henrique <pyter.dev@gmail.com>
2021-07-04 22:17:45 -0300

    light and dark themes created

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#835afb',
+    background: '#29292e',
+    font: '#f8f8f8',
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#835afb',
+    background: '#f8f8f8',
+    font: '#29292e',
+  },
+};
