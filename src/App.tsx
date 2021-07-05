666255690ab21fe6c35eab9c6d3e584cfd1c2a2e
Pedro Henrique <pyter.dev@gmail.com>
2021-07-04 22:28:16 -0300

    now the global style and theme provider is in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
@@ -7,18 +8,24 @@ import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
+import GlobalStyle from './styles/global';
+import light from './styles/themes/light';
+
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
-          <Route path="/admin/rooms/:id" component={AdminRoom} />
-        </Switch>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <ThemeProvider theme={light}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/rooms/new" component={NewRoom} />
+            <Route path="/rooms/:id" component={Room} />
+            <Route path="/admin/rooms/:id" component={AdminRoom} />
+          </Switch>
+        </AuthContextProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 
 import { Toaster } from 'react-hot-toast';
 
@@ -8,16 +7,10 @@ import App from './App';
 
 import './services/firebase';
 
-import GlobalStyle from './styles/global';
-import light from './styles/themes/light';
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={light}>
-      <GlobalStyle />
-      <App />
-      <Toaster />
-    </ThemeProvider>
+    <App />
+    <Toaster />
   </React.StrictMode>,
   document.getElementById('root'),
 );
