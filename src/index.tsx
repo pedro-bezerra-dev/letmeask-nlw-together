e42aa756173ef2063451075fd49a0206da530ab3
Pedro Henrique <pyter.dev@gmail.com>
2021-07-04 22:16:40 -0300

    global theme and theme provider imported and added to file index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { Toaster } from 'react-hot-toast';
 
@@ -7,12 +8,16 @@ import App from './App';
 
 import './services/firebase';
 
-import './styles/global.scss';
+import GlobalStyle from './styles/global';
+import light from './styles/themes/light';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <ThemeProvider theme={light}>
+      <GlobalStyle />
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
