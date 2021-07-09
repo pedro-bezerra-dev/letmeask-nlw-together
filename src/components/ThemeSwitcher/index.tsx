ca2032bf0f4a95dffebfc834e37814320722a968
Pedro Henrique <pyter.dev@gmail.com>
2021-07-09 15:36:48 -0300

    ThemeSwitcher component styled

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -1,4 +1,7 @@
 import { HTMLAttributes } from 'react';
+import cn from 'classnames';
+
+import { useTheme } from '../../hooks/useTheme';
 
 import { ThemeSwitcherStyled } from './styles';
 
@@ -8,10 +11,24 @@ import darkModeIcon from '../../assets/images/dark-mode.svg';
 type ThemeSwitcherProps = HTMLAttributes<HTMLButtonElement>
 
 export function ThemeSwitcher(props: ThemeSwitcherProps): JSX.Element {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <ThemeSwitcherStyled type="button" aria-label="Trocar tema" {...props}>
-      <img src={lightModeIcon} alt="Alterar para tema claro" />
-      <img src={darkModeIcon} alt="Alterar para tema escuro" />
+    <ThemeSwitcherStyled
+      type="button"
+      aria-label="Trocar tema"
+      onClick={toggleTheme}
+      className={cn(
+        'theme-switcher',
+        { light: theme.title === 'light' },
+        { dark: theme.title === 'dark' },
+      )}
+      {...props}
+    >
+      <div>
+        <img src={lightModeIcon} alt="Alterar para tema claro" className="light-icon" />
+        <img src={darkModeIcon} alt="Alterar para tema escuro" className="dark-icon" />
+      </div>
     </ThemeSwitcherStyled>
   );
 }
