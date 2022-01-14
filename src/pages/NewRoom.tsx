a2413b91e4ad59cc3d8cc80abf4af0a915090d7d
Pedro Henrique <pyter.dev@gmail.com>
2022-01-13 21:00:43 -0300

    fix: a room creator is now redirected to the admin page

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -30,7 +30,7 @@ export function NewRoom(): JSX.Element {
       authorId: user?.id,
     });
 
-    history.push(`/rooms/${firebaseRoom.key}`);
+    history.push(`/admin/rooms/${firebaseRoom.key}`);
   }
 
   return (
